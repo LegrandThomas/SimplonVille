a201c65aef1c54daf3526919e08af25ce2793c23
Thomas Legrand <pro.legrand.thomas@gmail.com>
2023-09-22 14:37:39 +0200

    fix/ cookibanner

--- Components/cookibanner.js
+++ Components/cookibanner.js
@@ -5,14 +5,12 @@ function CookieBanner() {
   const [modalVisible, setModalVisible] = useState(true);
 
   const acceptCookies = () => {
-    // Mettez ici votre logique pour accepter les cookies
-    // Par exemple, enregistrez l'acceptation dans l'état global ou AsyncStorage
+alert('cookies acceptés');
     setModalVisible(false);
   };
 
   const refuseCookies = () => {
-    // Mettez ici votre logique pour refuser les cookies
-    // Par exemple, enregistrez le refus dans l'état global ou AsyncStorage
+    alert('cookies refusés');
     setModalVisible(false);
   };
 
@@ -22,14 +20,13 @@ function CookieBanner() {
       transparent={true}
       visible={modalVisible}
       onRequestClose={() => {
-        // Mettez ici votre logique si l'utilisateur ferme la modal sans cliquer sur Accepter ou Refuser
+       
         setModalVisible(false);
       }}
     >
       <View style={styles.centeredView}>
         <View style={styles.modalView}>
           <Text>Nous utilisons des cookies de suivi pour comprendre comment vous utilisez le produit</Text>
-          <Text>et aidez-nous à l'améliorer.</Text>
           <Text>Veuillez accepter les cookies pour nous aider à améliorer.</Text>
           <Button title="Accepter les cookies" onPress={acceptCookies} />
           <Button title="Refuser les cookies" onPress={refuseCookies} />
